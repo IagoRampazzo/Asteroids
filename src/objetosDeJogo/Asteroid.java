76a27251c90ec6aaee14b5aa745f6fa713569474
Iago Rampazzo <iagorampazzo@hotmail.com>
2014-09-14 17:13:33 -0300

    Classe Asteroid atualizada
    Adicionado método "morrer"

--- src/objetosDeJogo/Asteroid.java
+++ src/objetosDeJogo/Asteroid.java
@@ -15,13 +15,32 @@ import javax.microedition.lcdui.Graphics;
 public class Asteroid extends ObjetoDoJogo{
     
     private static String IMAGEM_ASTEROID = "images/";
+    private static int[] QUANTOS_FILHOS_GERA = {2,2,0};    //1grande=>2 médios;1médio=>2 pequenos;
+    private int tipoAsteroid; //0 = MAIOR, 1 = Médio, 2 = menor
+    private boolean vivo;
     
-    // QUAL = 0; Pequeno
-    // QUAL = 1; Medio
-    // QUAL = 2; Grande
-    
-    public Asteroid(int posicaoX, int posicaoY, int raio, double angulo,int qual) {
+    public Asteroid(int tipo,int posicaoX, int posicaoY, int raio, double angulo) {
         super(posicaoX, posicaoY, raio, angulo,IMAGEM_ASTEROID);
-        IMAGEM_ASTEROID += qual + ".png";
+        IMAGEM_ASTEROID += tipo + ".png";
+        this.tipoAsteroid = tipo;
+        this.vivo = true;
     }
+    
+    /**
+     * 
+     * @return retorna os filhos do Asteroid, ou seja, os novos que irão surgir
+     */
+    public Asteroid[] morrer(){
+        vivo = false;
+        int quantoGera = QUANTOS_FILHOS_GERA[tipoAsteroid]; //quantos asteroids irao gerar caso seja atingido
+        int tipoFilho=tipoAsteroid+1;
+        Asteroid[] filhos = new Asteroid[quantoGera];
+        double anguloAux = 30;
+        for (int i=0;i<quantoGera;i++,anguloAux*=-1){
+            Asteroid novo = new Asteroid(tipoFilho,getX(),getY(),getRaio()/2,getAngulo()+anguloAux);
+            filhos[i] = novo;
+        }
+        return filhos;
+    }
+    
 }
